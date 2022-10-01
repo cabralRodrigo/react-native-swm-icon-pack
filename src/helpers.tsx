40ed74ce8b4c4d407d2f9b6315b1f8a0ae144c0e
Rodrigo Cabral <rodrigo.cabral@ebankit.com>
2022-10-01 10:25:50 -0300

    Adding option to set the icon fillcolor.

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,15 +1,16 @@
 import React, { forwardRef, memo, VFC } from 'react';
 import { StyleProp, View, ViewStyle } from 'react-native';
-import Svg from 'react-native-svg';
+import Svg, { Color } from 'react-native-svg';
 
 type IconProps = {
-    style?: StyleProp<ViewStyle>
+    style?: StyleProp<ViewStyle>,
+    fillColor?: Color
 }
 
 export function createIcon<TProps>(Component: VFC<TProps>) {
     const wrapper = forwardRef<View, TProps & IconProps>((props, ref) => (
         <View ref={ref} style={[{ aspectRatio: 1, width: 24, height: 24 }, props.style]}>
-            <Svg viewBox="0 0 24 24" fill="none">
+            <Svg viewBox="0 0 24 24" fill={props.fillColor ?? "none"}>
                 <Component {...props} />
             </Svg>
         </View>
